1d8b294bd2a6242984453110ca64d9a8e1f9b1c6
Jordan <Jordforb@gmail.com>
2022-04-07 11:48:30 -0400

    basic gui

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from utility import p,v,hp,sc,s,klax
 import units as u
 from DiceClass import D
+import PySimpleGUI as sg
 
 #instantiate units, variables          
 p1,e1= u.Player(),u.Enemy()
@@ -19,6 +20,16 @@ klax("#####")
 p()
 sc(p1,e1)
 p()
+
+###gui###
+i=0
+
+layout=[
+        [sg.Text( "hello" )],
+        [sg.Text( i, k='-int-' )],
+        [sg.Button( "Foo", k='-FOO-' )]
+        ]
+
 while hp(e1)>0:
     turn+=1
     p("turn "+s(turn))
@@ -27,6 +38,23 @@ while hp(e1)>0:
     p()
     sc(p1,e1)
     p()
+
+win = sg.Window('Roll').Layout(layout)
+
+while True:
+    #event, values
+    e, v = win.Read() 
+    
+    if e == sg.WIN_CLOSED or e == 'Close':
+        break 
+    
+    if e == '-FOO-':
+        i+=1
+        win['-int-'].update(i)
+
+win.Close()
+  
+###gui###  
 
 klax("########")
 klax("GAMEOVER")
